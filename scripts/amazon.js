7d41c384643823c359f6408f2978b47365151b22
buildwithanas <anasamoo0704@gmail.com>
2026-08-31 22:52:48 +0100

    Add Amazon Projects

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars; 
+  }
+}];
