1f89838db75f185888527bc6749d610440d5f033
buildwithanas <anasamoo0704@gmail.com>
2026-09-02 22:31:47 +0100

    Add Amazon Projects

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} 
+import {cart} from '../data';
 /*const products = [{
   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
