34be3f871357af852d223b9aa0ac611ec551d373
buildwithanas <anasamoo0704@gmail.com>
2026-09-03 23:04:19 +0100

    Add Amazon Projects

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,8 @@
 import {cart} from '../data/cart.js';
 
 cart.forEach((cartItem) => {
-
-  <div class="cart-item-container">
+  `
+    <div class="cart-item-container">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -30,4 +30,50 @@ cart.forEach((cartItem) => {
                   </span>
                 </div>
               </div>
-};
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="delivery-option-2">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked class="delivery-option-input"
+                    name="delivery-option-2">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="delivery-option-2">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  `
+});
