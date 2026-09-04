7cbe1a379ad133be3ebc9dfd4b7704c8d3805afd
buildwithanas <anasamoo0704@gmail.com>
2026-09-04 22:57:57 +0100

    Add Amazon E-commerce Website Checkout Page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = JSON.parse localStorage.getItem('cart')
+export const cart = JSON.parse (localStorage.getItem('cart'));
 
 [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
