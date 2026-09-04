9c5f7d9959f1d2bd89098facb42ddff8aeb28050
buildwithanas <anasamoo0704@gmail.com>
2026-09-04 22:44:12 +0100

    Add Amazon E-commerce Website Checkout Page

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,10 @@
-export const cart = [
-  {
+export const cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-  }
-];
+  }];
 
 function saveToStorage() {
   localStorage.setItem('CART', JSON.stringify(cart));
@@ -37,6 +35,7 @@ export function removeFromCart(productId) {
       newCart.push(cartItem);
     }
   });
+
   cart = newCart;
  // cart.length = 0;
   //cart.push(...newCart);
