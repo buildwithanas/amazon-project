ea890d77fd55268dde25898714ebf4915f19dd53
buildwithanas <anasamoo0704@gmail.com>
2026-09-06 22:20:38 +0100

    Add Amazon Checkout Page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -102,6 +102,6 @@ document.querySelectorAll('.js-delete-link')
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       );
-      console
+      container.remove();
     });
   });
